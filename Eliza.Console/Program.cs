17806cfb835ea9355ccbbb3bceda608864de389e
Martin Davy <mjdavy@hotmail.com>
2016-08-30 17:33:42 -0400

    Initial commit

---
+++ Eliza.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eliza.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ElizaMain eliza = new ElizaMain();
+
+            while(true)
+            {
+                var input = System.Console.ReadLine();
+                var output = eliza.ProcessInput(input);
+                System.Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ Eliza.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eliza.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ElizaMain eliza = new ElizaMain();
+
+            foreach (var s in new string[] { "Hello", "Well, and you?", "I'm implementing some weird project.", "Yes, can you give me technical advice?", "Thank you.", "Bye." })
+            {
+                Console.WriteLine("> {0}", s);
+                Console.WriteLine("< {0}", eliza.ProcessInput(s));
+            }
+        }
+    }
+}
